b5bab7cf89c696a5f680a5811708e2e6a99f4ff8
Shirllie-Apiyo <apraise320@gmail.com>
2021-11-30 01:13:45 +0300

    form validation

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -9,34 +9,13 @@ import { FormGroup,  FormBuilder } from '@angular/forms';
 })
 export class LogInComponent implements OnInit {
   loginForm!:FormGroup
-  // email = new FormControl('', Validators.required );
-  // username = new FormControl('', Validators.required );
-  // password = new FormControl('', Validators.required );
-
-
-  // getErrorMessage() {
-  //   if (this.email.hasError('required')) {
-  //     return 'You must enter a value';
-  //   }
-
-  //   return this.email.hasError('email') ? 'Not a valid email' : '';
-  // }
-
-  // getErrorMessage2() {
-  //   if (this.username.hasError('required')) {
-  //     return 'Enter a valid Password';
-  //   }
-
-  //   return this.username.hasError('username') ? 'Password must contain, letters numbers and symbols' : '';
-  // }
-
 
   constructor(private fb:FormBuilder) {this.createForm(); }
   createForm(){
     this.loginForm =this.fb.group({
-      username:['',Validators.required],
-      email:['',Validators.required],
-      password:['',Validators.required],
+      username:['',Validators.required ],
+      email:['',[Validators.required,Validators.email] ],
+      password:['',Validators.required ],
     });
   }
 
